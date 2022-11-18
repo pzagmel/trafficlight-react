e9e4302be008a450ecc04557efc35c963faa0f38
pzagmel <pedro.zagalm@gmail.com>
2022-11-18 15:03:15 +0000

    first commit

---
+++ src/js/component/TrafficLight.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+//include images into your bundle
+import rigoImage from "../../img/rigo-baby.jpg";
+
+//create your first component
+const TrafficLight = () => {
+  const [color, setColor] = useState("red");
+
+  return (
+    <div className="traficlight">
+      <div
+        onClick={() => setColor("red")}
+        className={"light red" + (color === "red" ? " glow" : "")}
+      ></div>
+
+      <div
+        onClick={() => setColor("yellow")}
+        className={"light yellow" + (color === "yellow" ? " glow" : "")}
+      ></div>
+
+      <div
+        onClick={() => setColor("green")}
+        className={"light green" + (color === "green" ? " glow" : "")}
+      ></div>
+    </div>
+  );
+};
+
+export default TrafficLight;
